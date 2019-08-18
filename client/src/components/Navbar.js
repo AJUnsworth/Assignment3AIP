e8feb8be806685585e267a9aebc801ecd575612d
Josh Chan <joshuachan35@gmail.com>
2019-08-18 21:46:43 +1000

    Add placeholder logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import Logo from '../images/Logo-01.png';
 
 export class Navbar extends React.Component {
     render() {
@@ -7,7 +8,7 @@ export class Navbar extends React.Component {
             <div class="navbar">
                 <div class="navbarContent">
                     {/* Logo */}
-                    <img src="#" alt="logo" class="navbarSizing"/>
+                    <img src={ Logo } alt="logo" class="navbarSizing"/>
                     {/* log in option if user unknown, account option if logged in */}
                     <button class="loginButton navbarSizing">Log in</button>
                 </div>
