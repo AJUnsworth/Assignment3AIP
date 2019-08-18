6c8711a71a695c40ab2a1023a785f80c7083aca9
AndrewJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2019-08-18 22:22:51 +1000

    Refactor Leaderboard component reference in ContentFrame

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
 import { ContentFrame } from './components/ContentFrame';
-import { Leaderboard } from './components/Leaderboard';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <ContentFrame>
-        <Leaderboard/>
-      </ContentFrame>
-      
+      <ContentFrame/>
     </div>
   );
 }
--- client/src/components/ContentFrame.js
+++ client/src/components/ContentFrame.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './ContentFrame.css';
+import { Leaderboard } from './Leaderboard';
 
 // Containment Function - taken from React.js.org documentation
 // https://reactjs.org/docs/composition-vs-inheritance.html
 
-export function ContentFrame(props) {
+export function ContentFrame() {
     return (
         <div className="contentFrame">
-            {props.children}
+            <Leaderboard/>
         </div>
     );
 }
