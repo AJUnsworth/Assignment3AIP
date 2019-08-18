025e345efc95cade3b6498178a8420f61d52e9c6
Josh Chan <joshuachan35@gmail.com>
2019-08-19 08:50:00 +1000

    Refactor reaction counter with bootstrap components and styles

--- client/src/components/ReactionCounter.js
+++ client/src/components/ReactionCounter.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './ReactionCounter.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComments } from '@fortawesome/free-solid-svg-icons'
+import { faComments, faHeart } from '@fortawesome/free-solid-svg-icons'
+import Badge from 'react-bootstrap/Badge'
 
 export class ReactionCounter extends React.Component {
     render() {
         return (
-            <div class="reactionCounter">
-                <FontAwesomeIcon icon={faComments} className="elementSpacing"/>
-                <label className="elementSpacing">Test</label>
-            </div>
+            <Badge pill variant="light">
+                <FontAwesomeIcon icon={faComments} className="iconSpacing lhsIcon text-primary"/>
+                8   
+                <FontAwesomeIcon icon={faHeart} className="iconSpacing rhsIcon text-danger"/>
+                57
+            </Badge>
         );
     }
 }
