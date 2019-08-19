b298de16f210f1720dca8208e210214a754485d6
AndrewJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2019-08-19 21:22:15 +1000

    Add ability to display leaderboard data with placeholder values

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,12 +3,20 @@ import { Navbar } from './Navbar';
 import { ContentFrame } from './ContentFrame';
 
 function Home() {
-  return (
-    <div>
-      <Navbar/>
-      <ContentFrame/>
-    </div>
-  );
+    const members = [
+        {name: 'Andrew', rank: '#1'},
+        {name: 'Bela', rank: '#2'},
+        {name: 'Chloe', rank: '#3'},
+        {name: 'James', rank: '#4'},
+        {name: 'Josh', rank: '#5'}
+    ];
+
+    return (
+        <div>
+            <Navbar/>
+            <ContentFrame members={members}/>
+        </div>
+    );
 }
 
 export default Home;
