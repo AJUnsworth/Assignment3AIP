133c9fe7aa096984aa9112d17cf93c6986b14f70
Josh Chan <joshuachan35@gmail.com>
2019-08-19 22:38:42 +1000

    Add imageGrid toolbar and button group functions

--- client/src/components/ImageGrid.js
+++ client/src/components/ImageGrid.js
@@ -1,25 +1,61 @@
 import React from 'react';
 import './ImageGrid.css';
 import { ImageFrame } from './ImageFrame';
-
-// Image Grid Logic using StackOverFlow logic
-// https://stackoverflow.com/questions/48418116/how-to-dynamically-populate-bootstrap-grids-using-reactjs
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 
 export class ImageGrid extends React.Component {
     render() {
         return (
             <div class="imageGrid">
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
-                <ImageFrame/>
+
+                <ButtonToolbar className="justify-content-between imageGridToolbar">
+                    <div>
+                        <h6>Add a thread...</h6>
+                        {/*upload button function taken from the following website:*/}
+                        {/*https://mdbootstrap.com/docs/react/forms/file-input/*/}
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroupFileAddon01">
+                                    Upload
+                                        </span>
+                            </div>
+                            <div className="custom-file">
+                                <input
+                                    type="file"
+                                    className="custom-file-input"
+                                    id="inputGroupFile01"
+                                    aria-describedby="inputGroupFileAddon01"
+                                />
+                                <label className="custom-file-label" htmlFor="inputGroupFile01">
+                                    Choose file
+                                        </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h6>Sort by</h6>
+                        <ButtonGroup>
+                            <Button variant="secondary">Latest</Button>
+                            <Button variant="secondary">Most Popular</Button>
+                            <Button variant="secondary">Trending</Button>
+                        </ButtonGroup>
+                    </div>
+                </ButtonToolbar>
+
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+                <ImageFrame />
+
             </div>
         );
     }
