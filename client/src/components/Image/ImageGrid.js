040d400b347faf238b331d3184d5242d5cede688
AJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2019-08-20 11:21:01 +1000

    Format Show More button placement to be below threads

--- client/src/components/Image/ImageGrid.js
+++ client/src/components/Image/ImageGrid.js
@@ -8,59 +8,58 @@ import Button from 'react-bootstrap/Button';
 class ImageGrid extends React.Component {
     render() {
         return (
-            <div class="imageGrid">
-
-                <ButtonToolbar className="justify-content-between imageGridToolbar">
-                    <div>
-                        <h6>Add a thread...</h6>
-                        {/*upload button function taken from the following website:*/}
-                        {/*https://mdbootstrap.com/docs/react/forms/file-input/*/}
-                        <div className="input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="inputGroupFileAddon01">
-                                    Upload
+            <div>
+                <div class="imageGrid">
+                    <ButtonToolbar className="justify-content-between imageGridToolbar">
+                        <div>
+                            <h6>Add a thread...</h6>
+                            {/*upload button function taken from the following website:*/}
+                            {/*https://mdbootstrap.com/docs/react/forms/file-input/*/}
+                            <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="inputGroupFileAddon01">
+                                        Upload
                                         </span>
-                            </div>
-                            <div className="custom-file">
-                                <input
-                                    type="file"
-                                    className="custom-file-input"
-                                    id="inputGroupFile01"
-                                    aria-describedby="inputGroupFileAddon01"
-                                />
-                                <label className="custom-file-label" htmlFor="inputGroupFile01">
-                                    Choose file
+                                </div>
+                                <div className="custom-file">
+                                    <input
+                                        type="file"
+                                        className="custom-file-input"
+                                        id="inputGroupFile01"
+                                        aria-describedby="inputGroupFileAddon01"
+                                    />
+                                    <label className="custom-file-label" htmlFor="inputGroupFile01">
+                                        Choose file
                                         </label>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div>
-                        <h6>Sort by</h6>
-                        <ButtonGroup>
-                            <Button variant="info">Latest</Button>
-                            <Button variant="secondary">Most Popular</Button>
-                            <Button variant="secondary">Trending</Button>
-                        </ButtonGroup>
-                    </div>
-                </ButtonToolbar>
+                        <div>
+                            <h6>Sort by</h6>
+                            <ButtonGroup>
+                                <Button variant="info">Latest</Button>
+                                <Button variant="secondary">Most Popular</Button>
+                                <Button variant="secondary">Trending</Button>
+                            </ButtonGroup>
+                        </div>
+                    </ButtonToolbar>
 
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                <ImageFrame />
-                
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                    <ImageFrame />
+                </div>
                 <div className="showMoreBtnContainer">
                     <Button variant="info">Show More</Button>
                 </div>
-
             </div>
         );
     }
