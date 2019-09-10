984f0765b3e27137b2a67bf34004a0385e73658e
AJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2019-09-10 12:28:58 +1000

    Fix homepage crashing issue when no user is set

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -10,7 +10,11 @@ class Home extends React.Component {
         //Convert user details from string to a JSON object to be able to access properties
         const user = JSON.parse(localStorage.getItem("User"));
         this.state = {
-            username: user.username
+            username: ""
+        }
+
+        if (user) {
+            this.setState({ username: user.username });
         }
     }
 
