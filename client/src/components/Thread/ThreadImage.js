91e9feb00a184ad793047fe7da7ca8245c7029a7
Josh Chan <joshuachan35@gmail.com>
2019-09-27 20:08:40 +0800

    Add imageremoved placeholder image

--- client/src/components/Thread/ThreadImage.js
+++ client/src/components/Thread/ThreadImage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ThreadImage.css";
-import ProfilePhoto from "../../images/profilepic.jpg";
+import PlaceholderImage from "../../images/imageremovedplaceholder.png";
 
 class ThreadImage extends React.Component {
     render() {
         return (
             <div className="threadImageContainer">
                 {/* eslint-disable-next-line */}
-                <img src={this.props.imageUrl ? this.props.imageUrl : ProfilePhoto} className="threadImage" />
+                <img src={this.props.imageUrl ? this.props.imageUrl : PlaceholderImage} className="threadImage" />
             </div>
 
         );
