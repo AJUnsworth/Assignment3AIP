b49fcb23982c0884451eecaa6f6c48af5618351d
Josh Chan <joshuachan35@gmail.com>
2019-10-05 21:38:29 +1000

    Revert "Remove profile image files and redundant user css references"
    This reverts commit eb80f77fbdfbdc4ae6ded02147dffb626db9aeb4.

---
+++ client/src/components/User/ProfilePicture.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import ProfilePhoto from "../../images/profilepic.jpg";
+import "./ProfilePicture.css";
+
+class ProfilePicture extends React.Component {
+    render() {
+        return (
+            <Link to="/user/">
+                <img src={ProfilePhoto} className="profilePicture" alt="profile" />
+            </Link>
+        );
+    }
+}
+
+export default ProfilePicture;
