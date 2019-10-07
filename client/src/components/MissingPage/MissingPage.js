6fe572a5d516becc3b78c5910407c5cb0965d044
Josh Chan <joshuachan35@gmail.com>
2019-10-07 18:07:38 +1100

    Add error page graphics

--- client/src/components/MissingPage/MissingPage.js
+++ client/src/components/MissingPage/MissingPage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFrown } from "@fortawesome/free-solid-svg-icons";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import ErrorImage from "../../images/404.png";
 
 import Navbar from "../Navbar/Navbar";
-
+import '../MissingPage/MissingPage.css'
 
 class User extends React.Component {
 
@@ -17,9 +16,7 @@ class User extends React.Component {
                 <Navbar {...this.props} />
                 <Row className="align-items-center">
                 <Col className="verticalColPadding textCentred">
-                    <FontAwesomeIcon icon={faFrown} className="fa-10x" />
-                    <h1>404 - Page not found</h1>
-                    <p>The page you are looking for does not exist.</p>
+                    <img src={ErrorImage} alt="error" className="errorImage"></img>
                     <Link to="/" ><Button variant="info">Go Home</Button></Link>
                 </Col>
                 </Row>
